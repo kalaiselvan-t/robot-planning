7f96806e68a4b744693c551fc76be298c65816dc
Placido <planora@hotmail.com>
2022-09-19 18:36:46 +0200

    Dockerfile fix

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -3,4 +3,9 @@ set -e
 
 # setup ros2 environment
 source "/opt/ros/humble/setup.bash" --
+cd ~/ros2_ws
+colcon build
+source "install/setup.bash" --
+echo "source ~/ros2_ws/install/setup.bash" >> ~/.bashrc
+export ROS_DOMAIN_ID=30
 exec "$@"
