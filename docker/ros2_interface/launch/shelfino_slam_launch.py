50061ce49191f53cc39c9875e9916f9148601139
Placido <planora@hotmail.com>
2022-09-27 19:30:36 +0200

    new SLAM map

--- docker/ros2_interface/launch/shelfino_slam_launch.py
+++ docker/ros2_interface/launch/shelfino_slam_launch.py
@@ -1,58 +1,85 @@
+# /* Author: Placido Falqueto */
+
 import os
 
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir
 
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time')
-    slam_params_file = LaunchConfiguration('slam_params_file')
-    rviz_config_file = LaunchConfiguration('rviz_config_file')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    shelfino_cartographer_prefix = get_package_share_directory(
+        'ros2_interface')
+    cartographer_config_dir = LaunchConfiguration(
+        'cartographer_config_dir',
+        default=os.path.join(shelfino_cartographer_prefix, 'config'))
+    configuration_basename = LaunchConfiguration(
+        'configuration_basename', default='shelfino.lua')
 
-    declare_use_sim_time_argument = DeclareLaunchArgument(
-        'use_sim_time',
-        default_value='true',
-        description='Use simulation/Gazebo clock')
-    declare_slam_params_file_cmd = DeclareLaunchArgument(
-        'slam_params_file',
-        default_value=os.path.join(get_package_share_directory("ros2_interface"),
-                                   'config', 'mapper_params_online_async.yaml'),
-        description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
-    declare_rviz_config_file_cmd = DeclareLaunchArgument(
-        'rviz_config_file',
-        default_value=os.path.join(get_package_share_directory("ros2_interface"),
-                                   'config', 'shelfino_slam.rviz'),
-        description='Full path to the ROS2 rviz config file')
+    resolution = LaunchConfiguration('resolution', default='0.05')
+    publish_period_sec = LaunchConfiguration('publish_period_sec', default='1.0')
 
-    start_async_slam_toolbox_node = Node(
-        parameters=[
-          slam_params_file,
-          {'use_sim_time': use_sim_time}
-        ],
-        package='slam_toolbox',
-        executable='async_slam_toolbox_node',
-        name='slam_toolbox',
-        output='screen')
-    
-    start_shelfino = Node(
-	package='ros2_interface',
-	executable='shelfino_node')
-	
-    start_rviz = Node(
-	package='rviz2',
-	executable='rviz2',
-	arguments=['-d', rviz_config_file])
+    rviz_config_dir = os.path.join(get_package_share_directory(
+        'ros2_interface'), 'config', 'shelfino_slam.rviz')
 
-    ld = LaunchDescription()
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'cartographer_config_dir',
+            default_value=cartographer_config_dir,
+            description='Full path to config file to load'),
+        DeclareLaunchArgument(
+            'configuration_basename',
+            default_value=configuration_basename,
+            description='Name of lua file for cartographer'),
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
 
-    ld.add_action(declare_use_sim_time_argument)
-    ld.add_action(declare_slam_params_file_cmd)
-    ld.add_action(declare_rviz_config_file_cmd)
-    ld.add_action(start_shelfino)
-    ld.add_action(start_rviz)
-    ld.add_action(start_async_slam_toolbox_node)
+        Node(
+	        package='ros2_interface',
+	        executable='shelfino_node',
+            name='shelfino_node'),
 
-    return ld
+        Node(
+            package='cartographer_ros',
+            executable='cartographer_node',
+            name='cartographer_node',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=['-configuration_directory', cartographer_config_dir,
+                       '-configuration_basename', configuration_basename]),
+
+        DeclareLaunchArgument(
+            'resolution',
+            default_value=resolution,
+            description='Resolution of a grid cell in the published occupancy grid'),
+
+        DeclareLaunchArgument(
+            'publish_period_sec',
+            default_value=publish_period_sec,
+            description='OccupancyGrid publishing period'),
+
+        Node(
+            package='cartographer_ros',
+            executable='cartographer_occupancy_grid_node',
+            name='cartographer_occupancy_grid_node',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=['-resolution', resolution,
+                       '-publish_period_sec', publish_period_sec]),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': use_sim_time}],
+            output='screen'),
+    ])
