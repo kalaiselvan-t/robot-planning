710eeebc2a07ec6d1d354b9ea3de844d64a0d753
Placido <planora@hotmail.com>
2022-09-29 18:46:36 +0200

    first commit of the simulator

---
+++ docker/shelfino_gazebo/launch/shelfino.launch.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+#
+# Authors: Placido Falqueto
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.actions import TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    world_file_name = 'Povo2_floor1.sdf'
+    world = os.path.join(get_package_share_directory('shelfino_gazebo'),
+                         'worlds', world_file_name)
+    launch_file_dir = os.path.join(get_package_share_directory('shelfino_gazebo'), 'launch')
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+
+    gazebo_models_path = os.path.join(get_package_share_directory('shelfino_gazebo'), 'models')
+    os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+            ),
+            launch_arguments={'world': world}.items(),
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+            ),
+        ),
+    
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=['-file', os.path.join(gazebo_models_path, 'shelfino', 'model.sdf'),
+                       '-entity', 'shelfino',
+                       '-x', '40',
+                       '-y', '17']
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([launch_file_dir, '/robot_state_publisher.launch.py']),
+            launch_arguments={'use_sim_time': use_sim_time}.items(),
+        ),
+    ])
