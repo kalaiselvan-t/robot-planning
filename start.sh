f1ab70ae49cc5d6877980b7665d42ad91d342b6a
Placido <planora@hotmail.com>
2022-10-11 15:43:12 +0200

    minor change

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 xhost +
-docker run --rm -it --name ros2 --network host --env="DISPLAY=host.docker.internal:0" -v /tmp/.X11-unix:/tmp/.X11-unix:rw pla10/ros2_humble:amd64 /bin/bash
+# docker run --rm -it --name ros2 --network host --env="DISPLAY=host.docker.internal:0" -v /tmp/.X11-unix:/tmp/.X11-unix:rw pla10/ros2_humble:amd64 /bin/bash
+docker run --rm -it --name ros2 --network host --env="DISPLAY=:0" -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v /dev/dri:/dev/dri pla10/ros2_humble:amd64 /bin/bash
