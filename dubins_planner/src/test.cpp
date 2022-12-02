8e072167a2deaa60c5f945affd5ee7c8e3b257c9
Kalaiselvan <im.kalaiselvan.t@gmail.com>
2022-12-02 12:24:23 +0000

    testing header files

--- dubins_planner/src/test.cpp
+++ dubins_planner/src/test.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include "test.h"
 
-double round_up(double value, int decimal_places) {
-    const double multiplier = std::pow(10.0, decimal_places);
-    return std::ceil(value * multiplier) / multiplier;
-}
+// struct cat c;
+
+// double round_up
+// (double value, int decimal_places)
+// {
+//     const double multiplier = std::pow(10.0, decimal_places);
+//     return std::ceil(value * multiplier) / multiplier;
+// }
 
 void add
 (int a, int b)
@@ -31,30 +36,22 @@ void divi
 	std::cout << "Division: " << a / b << "\n";
 }
 
-double sinct
-(double t)
-{
-	double out;
-	if
-	(std::abs(t) < 0.002)
-	{
-		out = 1 - std::pow(t, 2.0/6.0) * (1 - std::pow(t,2.0/20.0));
-	}
-	else
-	{
-		out = std::sin(t) / t;
-	}
+// double sinct
+// (double t)
+// {
+// 	double out;
+// 	if
+// 	(std::abs(t) < 0.002)
+// 	{
+// 		out = 1 - std::pow(t, 2.0/6.0) * (1 - std::pow(t,2.0/20.0));
+// 	}
+// 	else
+// 	{
+// 		out = std::sin(t) / t;
+// 	}
 
-	return out;
-}
-
-
-struct cat
-{
-	double a;
-	double b;
-	double c;
-};
+// 	return out;
+// }
 
 void test_struc
 (cat *inp)
@@ -64,74 +61,10 @@ void test_struc
 	inp->c = 3.0;
 }
 
-void pointer_test
-(int &a, double &b, char &c)
-{
-	a = 10;
-	b = 3.0;
-	c = 'k';
-}
-
-int main
-()
-{	
-	//Create function pointers
-	void (*fn1)(int, int);
-	void (*fn2)(int, int);
-	void (*fn3)(int, int);
-	void (*fn4)(int, int);
-
-	//Vector definiton
-	std::vector<void (*)(int, int)> my_func;
-
-	//Initialization
-	fn1 = &add;
-	fn2 = &sub;
-	fn3 = &prod;
-	fn4 = &divi;
-
-	//Add to vector
-	my_func.push_back(fn1);
-	my_func.push_back(fn2);
-	my_func.push_back(fn3);
-	my_func.push_back(fn4);
-	
-	//my_func[0](2,3);
-
-	for(int i = 0; i < my_func.size(); i++)
-	{
-		my_func[i](2,3);
-	}
-
-	//std::cout << "sinct :" << sinct(0.001) << "\n";
-	
-
-	cat c;
-
-	test_struc(&c);
-
-	std::cout << "A: " << c.a << "\n";
-	std::cout << "B: " << c.b << "\n";
-	std::cout << "C: " << c.c << "\n";
-
-	double tt = 3.4987564;
-
-	double result = round_up(tt, 4);
-	std::cout << "round_up: " << result << std::endl;
-
-	int aa;
-	double bb;
-	char cc;
-
-	int jj = 9;
-
-	int *jj_add = &jj;
-
-	*jj_add = 10;
-
-	pointer_test(aa, bb, cc);
-
-	std::cout << jj_add << " " << *jj_add << " " << jj << std::endl;
-
-	return 0;
-}
+// void pointer_test
+// (int &a, double &b, char &c)
+// {
+// 	a = 10;
+// 	b = 3.0;
+// 	c = 'k';
+// }
---
+++ dubins_planner/src/test.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+struct cat
+{
+	double a;
+	double b;
+	double c;
+};
+
+// double round_up(double, int);
+
+void add(int, int);
+
+void sub(int, int);
+
+void prod(int, int);
+
+void divi(int, int);
+
+// double sinct(double);
+
+void test_struc(cat *);
+
+// void pointer_test(&int, &double, &char);
