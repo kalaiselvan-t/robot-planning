28cb395f7e8adcd817efeb6518322dca26f40cab
Kalaiselvan <im.kalaiselvan.t@gmail.com>
2022-12-24 12:30:19 +0000

    obs data types added

--- git-push.sh
+++ git-push.sh
@@ -3,5 +3,6 @@ branch_name=${branch_name##refs/heads/}
 git add .
 git commit -m "$1"
 git push github $branch_name
-send -- "kalaiselvan-t"
+expect "Username for 'https://github.com': "
+send -- "kalaiselvan-t\r"
 echo "pushed to $branch_name"
